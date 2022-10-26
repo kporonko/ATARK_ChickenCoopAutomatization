ab4c78611b9a4c8265929f1e68ace3633d922f70
KeeYoo <poronko2045@gmail.com>
2022-10-26 18:05:42 +0300

    Small rest fix

--- Backend/Backend/Controllers/ProfileController.cs
+++ Backend/Backend/Controllers/ProfileController.cs
@@ -16,7 +16,7 @@ namespace Backend.Controllers
         }
 
         [HttpGet]
-        [Route("Login")]
+        [Route("login")]
         public ActionResult<ProfileDto> LoginUser(string login, string password)
         {
             var profile = _profileService.Login(login, password);
@@ -26,7 +26,7 @@ namespace Backend.Controllers
         }
 
         [HttpPost]
-        [Route("Register")]
+        [Route("register")]
         public IActionResult RegisterNewUser(ProfileRegister profileRegister)
         {
             var response = _profileService.RegisterProfile(profileRegister);
