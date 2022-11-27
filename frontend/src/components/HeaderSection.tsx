2cc953426e997ae538cdfc64c356c4e3cb0d7ad7
KeeYoo <poronko2045@gmail.com>
2022-11-27 03:24:06 +0200

    Header section: localization added

--- frontend/src/components/HeaderSection.tsx
+++ frontend/src/components/HeaderSection.tsx
@@ -1,10 +1,27 @@
 import React from 'react';
+import LocalizedStrings from 'react-localization';
+import {inspect} from "util";
 
 const HeaderSection = (props: {text: string}) => {
+
+    let strings = new LocalizedStrings({
+        en:{
+            myCoops:"My Coops",
+        },
+        ru: {
+            myCoops:"Мои Курятники",
+        }
+    });
+
+    const localize = (text: string) => {
+        if (text === 'My Coops')
+            return strings.myCoops;
+    }
+
     return (
         <div className='header-section-wrapper'>
             <h2 className='header-section-text'>
-                {props.text}
+                {localize(props.text)}
             </h2>
         </div>
     );
