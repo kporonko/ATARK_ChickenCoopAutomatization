2a37b3bd3605f3b0622c911f3fdee8a59acb3510
KeeYoo <poronko2045@gmail.com>
2022-11-27 20:05:33 +0200

    CORS added to backend, assets added

--- Backend/Backend/Program.cs
+++ Backend/Backend/Program.cs
@@ -7,6 +7,17 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      builder =>
+                      {
+                          builder.WithOrigins("http://localhost:3000", "http://localhost:3001", "http://localhost:8080").AllowAnyHeader().AllowAnyMethod();
+                      });
+});
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -16,7 +27,8 @@ builder.Services.AddDbContext<ApplicationContext>();
 builder.Services.AddTransient<IProfileService, ProfileService>();
 builder.Services.AddTransient<ICoopService, CoopService>();
 
-var app = builder.Build();
+var app = builder.Build(); 
+app.UseCors(MyAllowSpecificOrigins);
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
