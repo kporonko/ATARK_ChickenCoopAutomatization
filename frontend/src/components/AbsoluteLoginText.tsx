b771c866c09b74dd91fde5d83510b1411ff8f4dc
KeeYoo <poronko2045@gmail.com>
2022-11-27 03:43:21 +0200

    Login page: localization added. Hover css fix

--- frontend/src/components/AbsoluteLoginText.tsx
+++ frontend/src/components/AbsoluteLoginText.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import LocalizedStrings from 'react-localization';
 
 const AbsoluteLoginText = () => {
+
+    let strings = new LocalizedStrings({
+        en:{
+            text:"Best application for coops tracking and chicken care",
+        },
+        ru: {
+            text:"Лучшее приложение для трекинга статистики курятников и заботы о курях",
+        }
+    });
+
     return (
         <div>
             <span className='main-header-login'>Chicken Coop</span>
-            <h2 className='text-header-login'>Best application for coops tracking and chicken care</h2>
+            <h2 className='text-header-login'>{strings.text}</h2>
         </div>
     );
 };
