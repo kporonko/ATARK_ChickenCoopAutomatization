fa2260257b1808ea0a0f3681d521b87421fddcdf
KeeYoo <poronko2045@gmail.com>
2022-11-27 03:29:29 +0200

    Navigation menu: localization added

--- frontend/src/components/NavMenu.tsx
+++ frontend/src/components/NavMenu.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import LocalizedStrings from 'react-localization';
 import {Link} from "react-router-dom";
 
 const NavMenu = (props: {indexActive: number}) => {
+
+    let strings = new LocalizedStrings({
+        en:{
+            myCoops:"My Coops",
+            about:"About App"
+        },
+        ru: {
+            myCoops:"Мои Курятники",
+            about:"О Приложении",
+        }
+    });
+
     return (
         <div className="nav-wrapper">
             <div className='icon'>
@@ -12,14 +25,14 @@ const NavMenu = (props: {indexActive: number}) => {
                 <div className='nav-element'>
                     <Link to={'/my-coops'} className='nav-link'>
                         <img className='nav-link-icon' src={require('../assets/7415381.png')} alt=""/>
-                        <h2 className={`nav-link-text ${props.indexActive === 0 && 'active-page'}`}>My Coops</h2>
+                        <h2 className={`nav-link-text ${props.indexActive === 0 && 'active-page'}`}>{strings.myCoops}</h2>
                     </Link>
                 </div>
 
                 <div className='nav-element'>
                     <Link to={'/about'} className='nav-link'>
                         <img className='nav-link-icon-2' src={require('../assets/about.png')} alt=""/>
-                        <h2 className={`nav-link-text ${props.indexActive === 1 && 'active-page'}`}>About</h2>
+                        <h2 className={`nav-link-text ${props.indexActive === 1 && 'active-page'}`}>{strings.about}</h2>
                     </Link>
                 </div>
             </div>
