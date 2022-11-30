f7e6a3de8c84470ae990fd7cc6fcf341ce7b9761
KeeYoo <poronko2045@gmail.com>
2022-11-30 03:44:43 +0200

    Frontend dto interfaces is fixed according to backend fixed models

--- frontend/src/interfaces/ICoop.tsx
+++ frontend/src/interfaces/ICoop.tsx
@@ -2,5 +2,7 @@ export interface ICoop{
     name: string,
     eggCollects: {string: number},
     eggsByWeek: number,
-    allFeedingsHistory: string[]
+    allFeedingsHistory: string[],
+    thermometerIp?: string,
+    thermometerApiKey?: string
 }
